32ae55f99ddb273f094e7b70257bc9625e50f2f5
Legend <jasonrfelipe@gmail.com>
2018-06-15 10:32:57 -0700

    added notes on what to do next and updated js and html

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -88,13 +88,21 @@ database.ref().once("value", function(snap) {
 });
 
 
-//What I want to do: 
-//Make two buttons, one for player 1 and another for player 2
+//Stuff to do:
+//User presses a button, they turn into player one or two
+//if player 1 is chosen: show PLAYER 1 ALREADY THERE
+//if player 2 is chosen: show PLAYER 2 ALREADY THERE
+
+//also updates the chatbox saying if a player is connected
+
 
 $('#playerOne').on("click",function(){
     console.log("hey this is player one")
     $('#instructions').text("You are Player One")
     $(".hands").show();
+
+    //fix this so it appends to the box when
+    $('#chatbox').append("<br>" + "Player One has joined")
 
 
 });
@@ -104,40 +112,65 @@ $('#playerTwo').on("click", function(){
     $('#instructions').text("You are Player Two")
     $(".hands").show();
 
+    //fix this so it appends to the box
+    $('#chatbox').append("<br>" + "Player Two has joined")
+
 
 });
-
-
-//User presses a button, they turn into player one or two
-//if player 1 is chosen: show PLAYER 1 ALREADY THERE
-//if player 2 is chosen: show PLAYER 2 ALREADY THERE
 
 
 //-------The Hands----------
-$("#rock").on("click", function (){
-    console.log("You Chose Rock")
-    $('#playerChoice').text("You Chose Rock!")
-    $('#waiting').text("Waiting for Opponent...")
+
+//Stuff to do:
+//Create Logic which is...
+//Paper Beats Rock, Rock Beats Scissors, Scissors Beats Paper
+//Once Player choses the other images disappear
 
 
-});
+//Maybe an if statement checking the alt of the image will do for the logic?
 
+$('.hands').on("click", function(){
 
-$("#paper").on("click", function (){
-    console.log("You Chose Paper")
-    $('#playerChoice').text("You Chose Paper!")
-    $('#waiting').text("Waiting for Opponent...")
-
-});
-
-
-$("#scissors").on("click", function (){
-    console.log("You Chose Scissors")
-    $('#playerChoice').text("You Chose Scissors!")
-    $('#waiting').text("Waiting for Opponent...")
-
+    $('.hands').hide();
+    $(this).show();
+    $('#playerChoice').text("You Chose " + this.alt + "!");
+    $('#waiting').text("Waiting for Opponent...");
 
 });
+
+//Just in case we still need this stuff below....
+
+// $("#rock").on("click", function (){
+//     console.log("You Chose Rock")
+//     $('#paper').hide();
+//     $('#scissors').hide();
+//     $('#playerChoice').text("You Chose Rock!");
+//     $('#waiting').text("Waiting for Opponent...");
+
+
+// });
+
+
+// $("#paper").on("click", function (){
+//     console.log("You Chose Paper")
+//     $('#rock').hide();
+//     $('#scissors').hide();
+//     $('#playerChoice').text("You Chose Paper!")
+//     $('#waiting').text("Waiting for Opponent...")
+
+
+// });
+
+
+// $("#scissors").on("click", function (){
+//     console.log("You Chose Scissors")
+//     $('#rock').hide();
+//     $('#paper').hide();
+//     $('#playerChoice').text("You Chose Scissors!")
+//     $('#waiting').text("Waiting for Opponent...")
+
+
+// });
 
 
 
